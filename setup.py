a85cbe63284aed51179d85e654b7185acc7d605f
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 23:52:46 +0300

    2020:12:03 23:52

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='env2json',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
